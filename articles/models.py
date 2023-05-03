2bbc2d8c47cce15f1e9ee9c28d21db9ad1d5e098
Kolya Goroh <fasolka9520@gmail.com>
2023-05-03 13:34:56 +0300

    Articles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=3000)
+    author = models.CharField(max_length=200)
+    like_count = models.IntegerField(default=0)
+
+    @admin.display(
+        boolean=True,
+        description="Is popular?",
+        ordering='like_count'
+    )
+
+    def is_popular(self):
+        return self.like_count >= 100
+
+    def __str__(self):
+        return self.title
+
--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.urls import reverse
+from .views import Article
 
-# Create your tests here.
+def crate_article(title, text, author, like_count):
+    return Article.objects.create(title=title, text=text, author=author, like_count=like_count)
+
+class ArticleTest(TestCase):
+    def test_empty_list(self):
+        article = self.client.get(reverse('articles:main'))
+        self.assertEquals(article.status_code, 200)
+        self.assertContains(article, 'There are not articles')
+
+    def test_new_list(self):
+        article = create_article('1', '2', '3', '4')
+        response = self.client.get(reverse('articles:main'))
+        self.assertEquals(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['articles'], [article])
+
+    def test_is_popylar(self):
+        article = Article(like_count=1000)
+        self.assertTrue(article.is_popular())
+
+    def test_is_no_popular(self):
+        article = Article(like_count=1)
+        self.assertFalse(article.is_popular())
+
+
+
+
+
