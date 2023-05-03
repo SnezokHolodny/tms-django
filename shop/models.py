ea9466e678c0184eeb20c3d8225762de7743b199
Kolya Goroh <fasolka9520@gmail.com>
2023-05-03 13:38:57 +0300

    Shop

---
+++ shop/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
---
+++ shop/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Category, Product
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'shop/index.html', {'categories': Category.objects.all()})
+
+def category_detail(request, category_id: int):
+    return render(request, 'shop/category_detail.html',{
+        'category': get_object_or_404(Category, id=category_id),
+    })
+
+def product_detail(request, product_id: int):
+    return render(request, 'shop/product_detail.html',{
+        'product': get_object_or_404(Product, id=product_id),
+    })
